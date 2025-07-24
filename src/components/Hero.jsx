b670fe98a2c55e8fc8b3bcf62804070dcc02a975
MuhammadHamidy <hamidym84@gmail.com>
2025-07-24 16:25:55 +0700

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,9 +10,10 @@ const Hero = () => {
         </h2>
         <p className="hero-subtitle">Exploring the Digital Universe</p>
         <p className="hero-description">
-          Hi, I'm Mu'ammal Hamidy — a Computer Science student and aspiring Front-End Developer 
-          passionate about exploring user interfaces, web ecosystems, and intelligent systems. This is my mission control. 
-          Let's launch into my work.
+          Hi, I'm Mu'ammal Hamidy — Computer Science student of Pertamina University with strong interest in Front End development and UI/UX design.
+          Experienced as an operating system lab assistant and working on web development projects with expertise in UI/UX design,
+          Flutter, and React JS. Proactive, fast learner, and skilled in teamwork and communication. Passionate about developing
+          skills and contributing to the success of the company.
         </p>
       </div>
       <div className="hero-image">
