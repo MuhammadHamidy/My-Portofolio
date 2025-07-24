b1f9edafeb84906d44c3ae48330fc2d4dc2820aa
MuhammadHamidy <hamidym84@gmail.com>
2025-07-24 16:18:57 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import SpaceBackground from './components/SpaceBackground';
 import Header from './components/Header';
 import Hero from './components/Hero';
